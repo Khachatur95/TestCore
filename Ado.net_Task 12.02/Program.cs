4f60f5ce92335559966388d71897c3b2212442d1
Khachatur Melkonyan <khachatur.melkonyan.95@gmail.com>
2020-02-12 14:10:25 +0400

    Add project files.

---
+++ Ado.net_Task 12.02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ado.net_Task_12._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
